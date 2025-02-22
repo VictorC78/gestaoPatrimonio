dd206a37db1052a754c9279d9cb4fc0aef0e6a9d
VictorC78 <victorcarlos0000000000@gmail.com>
2025-02-22 11:05:50 -0300

    tudo_certo

--- gestao_bens/dashboard/urls.py
+++ gestao_bens/dashboard/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import CategoriaListView, DepartamentoListView
+from .views import CategoriaListView, DepartamentoListView, FornecedorListView, MovimentacaoListView
 
 urlpatterns = [
-    path('categorias/', CategoriaListView.as_view(), name='lista_categorias'),  # Verifique se o nome da URL está correto
-    path('departamentos/', DepartamentoListView.as_view(), name='lista_departamentos'),  # Verifique se o nome da URL está correto
+    path('categorias/', CategoriaListView.as_view(), name='lista_categorias'),  
+    path('departamentos/', DepartamentoListView.as_view(), name='lista_departamentos'), 
+    path('fornecedores/', FornecedorListView.as_view(), name='lista_fornecedores'),  
+    path('movimentacoes/', MovimentacaoListView.as_view(), name='lista_movimentacoes'),  
+    
+    
     
 ]
--- gestao_bens/dashboard/views.py
+++ gestao_bens/dashboard/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 import requests
 from django.db.models import Count, Sum
 from django.views.generic import ListView
-from api.models import Categoria, Departamento
+from api.models import Categoria, Departamento, Fornecedor, Movimentacao
 
 
 class IndexView(TemplateView): 
@@ -29,23 +29,23 @@ class CategoriaListView(ListView):
     context_object_name = 'categorias'
 
     def get_queryset(self):
-        # Obtém todas as categorias e anota a quantidade de bens e o valor total
+        
         return Categoria.objects.annotate(
-            qtd_bens=Count('bem'),  # Conta a quantidade de bens na categoria
-            valor_total=Sum('bem__valor_aquisicao')  # Soma o valor dos bens na categoria
+            qtd_bens=Count('bem'),  
+            valor_total=Sum('bem__valor_aquisicao') 
         )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        # Aqui, fazemos uma requisição à API se necessário (opcional)
+        
         url = 'http://127.0.0.1:8000/api/categorias/'
         response = requests.get(url)
 
-        # Adiciona as categorias da API ao contexto, caso a resposta seja bem-sucedida
+        
         if response.status_code == 200:
-            categorias_api = response.json()  # Extrai os dados da API
-            context['categorias_api'] = categorias_api  # Adiciona ao contexto para exibição
+            categorias_api = response.json() 
+            context['categorias_api'] = categorias_api  
 
         return context
     
@@ -54,23 +54,74 @@ class DepartamentoListView(ListView):
     context_object_name = 'departamentos'
 
     def get_queryset(self):
-        # Obtém todas as departamentos e anota a quantidade de bens e o valor total
+        
         return Departamento.objects.annotate(
-            qtd_bens=Count('bem'),  # Conta a quantidade de bens na departamento
-            valor_total=Sum('bem__valor_aquisicao')  # Soma o valor dos bens na departamento
+            qtd_bens=Count('bem'), 
+            valor_total=Sum('bem__valor_aquisicao')  
         )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        # Aqui, fazemos uma requisição à API se necessário (opcional)
+       
         url = 'http://127.0.0.1:8000/api/departamentos/'
         response = requests.get(url)
 
-        # Adiciona as departamentos da API ao contexto, caso a resposta seja bem-sucedida
+        
         if response.status_code == 200:
-            departamentos_api = response.json()  # Extrai os dados da API
-            context['departamentos_api'] = departamentos_api  # Adiciona ao contexto para exibição
+            departamentos_api = response.json()  
+            context['departamentos_api'] = departamentos_api  
 
         return context
+
+class FornecedorListView(ListView):
+    template_name = 'fornecedor/lista_fornecedores.html'
+    context_object_name = 'fornecedores'
+
+    def get_queryset(self):
+        
+        return Fornecedor.objects.annotate(
+            qtd_bens=Count('bem'),  
+            valor_total=Sum('bem__valor_aquisicao')  
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        
+        url = 'http://127.0.0.1:8000/api/fornecedores/'
+        response = requests.get(url)
+
+        
+        if response.status_code == 200:
+            fornecedores_api = response.json()  
+            context['fornecedores_api'] = fornecedores_api  
+
+        return context
+    
+class MovimentacaoListView(ListView):
+    template_name = 'movimentacao/lista_movimentacoes.html'
+    context_object_name = 'movimentacoes'
+
+    def get_queryset(self):
+        
+        return Movimentacao.objects.annotate(
+            qtd_bens=Count('bem'),  
+            valor_total=Sum('bem__valor_aquisicao')  
+        )
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        
+        url = 'http://127.0.0.1:8000/api/movimentacaos/'
+        response = requests.get(url)
+
+        
+        if response.status_code == 200:
+            movimentacoes_api = response.json() 
+            context['movimentacaos_api'] = movimentacoes_api  
+
+        return context
+
 
